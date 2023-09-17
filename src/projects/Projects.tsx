4718a08a728bcea1cce750a1fc91065a9472c435
digitrom <roma.bobrovski@gmail.com>
2023-09-17 23:39:02 +0200

    Making styles

--- src/projects/Projects.tsx
+++ src/projects/Projects.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import style from './Projects.module.css'
 import  styleContainer from '../common/styles/Container.module.css'
 import Project from "./project/Project";
+import Title from "../common/components/title/Title";
 
 const Projects = () => {
     return (
         <div className={style.projectsBlock}>
             <div className={`${styleContainer.container} ${style.projectsContainer}`}>
-                <h2 className={styleContainer.title}>My Projects</h2>
+                <div className={style.title}>
+                  <Title title={'My projects'}/>
+                </div>
                 <div className={style.projects}>
                     <Project projectImage={''} title={'Counter'} description={'counter js'}/>
                     <Project projectImage={''} title={'social network'} description={'social network typescript social network typescript social network typescript'}/>
--- src/skills/Skills.tsx
+++ src/skills/Skills.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import Skill from "./skill/Skill";
 import style from "./Skills.module.css"
 import styleContainer from "../common/styles/Container.module.css"
+import Title from "../common/components/title/Title";
 
 const Skills = () => {
     return (
         <div className={style.skillsBlock}>
             <div className={`${styleContainer.container} ${style.skillsContainer}`}>
-                <h2 className={style.title}>My Skills</h2>
+               <Title title={'My Skills'}/>
                 <div className={style.skills}>
                     <Skill
                         title={'React'}
